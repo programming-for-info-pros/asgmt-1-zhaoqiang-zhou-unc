2ede4ddb74cad7b8bc48a0a404092b200371a8b7
Zhaoqiang Zhou <zhzhou@unc.edu>
2024-08-29 14:30:36 +0000

    Print Hello World

--- hello_world.py
+++ hello_world.py
@@ -1,6 +1,8 @@
 # define a function to print 'Hello, World!'
-def hello_world():
-    print("Hello, World!")
+#def hello_world():
+#    print("Hello, World!")
 
 # run the function to print 'Hello, World!'
-hello_world()
+#hello_world()
+
+print("Hello, World!")
