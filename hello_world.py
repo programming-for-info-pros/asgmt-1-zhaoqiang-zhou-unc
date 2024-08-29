03f882ce347eb86af7b7e9f7dc066944ff58513d
Zhaoqiang Zhou <zhzhou@unc.edu>
2024-08-29 14:32:33 +0000

    change hello_world.py by defining a function to print hello world

--- hello_world.py
+++ hello_world.py
@@ -1,8 +1,6 @@
 # define a function to print 'Hello, World!'
-#def hello_world():
-#    print("Hello, World!")
+def hello_world():
+    print("Hello, World!")
 
 # run the function to print 'Hello, World!'
-#hello_world()
-
-print("Hello, World!")
+hello_world()
